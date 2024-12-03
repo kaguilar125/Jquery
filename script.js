ccc04fe65c73f6124a8072d9dfe77c09d94e095b
Kevin <125kaguilarmartinez@frhsd.com>
2024-12-03 12:53:59 -0500

    wodajiofha

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-$('li').hide();
+// $('li').hide();
 
+// function myFunction(){
+//     $('li').hide().fadeIn(500);
 
+//     console.log($('#free').html());
+//     $('li.hot').css("color", "red");
+//     $('li.cold').css("color", "blue");
 
-function myFunction(){
-    $('li').hide().fadeIn(500);
+// }
 
-    console.log($('#free').html());
-    $('li.hot').css("color", "red");
-    $('li.cold').css("color", "blue");
+document.addEventListener('DOMContentLoaded', ()=>{
 
+CaroS.onclick = function(e) {
+    clearInterval(CaroAutoplay);
+    console.log(e.type + ' mouse detected');
 }
+
+hammer.on('tap', function(e) {
+    clearInterval(CaroAutoplay);
+    console.log(e.type + ' gesture detected');
+});
+
+})
