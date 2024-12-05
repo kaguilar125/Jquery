6d303c1f4f40a9b6044db3a350261daba9be9fb2
Kevin <125kaguilarmartinez@frhsd.com>
2024-12-05 14:05:16 -0500

    audhawhdjkwahd

--- script.js
+++ script.js
@@ -11,14 +11,55 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-CaroS.onclick = function(e) {
-    clearInterval(CaroAutoplay);
-    console.log(e.type + ' mouse detected');
-}
 
-hammer.on('tap', function(e) {
-    clearInterval(CaroAutoplay);
-    console.log(e.type + ' gesture detected');
+    // Slider Begin
+    const CaroS = document.querySelector('.Carousel-slider');
+    const CaroSlider = new MicroSlider (CaroS, { indicators: true, IndicatorText:''});
+    const hammer = new Hammer (CaroS);
+    const CarosTimer = 2000;
+    let CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+   
+    //---- Mouseenter Event
+    CaroS.onmouseenter = function(e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + 'mouse detected');
+    }
+
+    //----- Mouseleave Event
+    CaroS.onmouseleave = function(e) {
+        clearInterval(CaroAutoplay);
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + 'mouse detected');
+        }
+
+    //------- Mouseclick event
+    CaroS.onclick = function(e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + 'mouse detected');
+    }
+
+    //------- Gesture Tap Event
+    hammer.on('tap', function(e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + 'gesture detected');
+    });
+
+    //----- Gesture Swipe Event
+    hammer.on('Swipe', function(e){
+        clearInterval(CaroAutoplay);
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + 'gesture detected');
+    });
+
+    let slideLink = document.querySelectorAll('.slider-item');
+    if (slideLink && slideLink !== null && slideLink.length > 0){
+        slideLink.forEach(el => el.addEventListener('click', e => {
+        e.preventDefault();
+        let href = el.dataset.href;
+        let target = el.dataset.target;
+        if (href !== '#') window.open(href, target);
+    }));
+//---- Slider End
+
+
 });
-
-})
