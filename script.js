0d66ade85806c2957021866317767398583fc389
Kevin <125kaguilarmartinez@frhsd.com>
2024-12-12 12:54:21 -0500

    dwajdkljwalkd

--- script.js
+++ script.js
@@ -9,57 +9,54 @@
 
 // }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-
-
+ocument.addEventListener('DOMContentLoaded', () => {
     // Slider Begin
     const CaroS = document.querySelector('.Carousel-slider');
-    const CaroSlider = new MicroSlider (CaroS, { indicators: true, IndicatorText:''});
-    const hammer = new Hammer (CaroS);
-    const CarosTimer = 2000;
+    const CaroSlider = new MicroSlider(CaroS, { indicators: true, IndicatorText: '' });
+    const hammer = new Hammer(CaroS);
+    const CaroSTimer = 2000;
     let CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
-   
-    //---- Mouseenter Event
-    CaroS.onmouseenter = function(e) {
-        clearInterval(CaroAutoplay);
-        console.log(e.type + 'mouse detected');
-    }
 
-    //----- Mouseleave Event
-    CaroS.onmouseleave = function(e) {
+    // Mouseenter Event
+    CaroS.onmouseenter = () => {
+        clearInterval(CaroAutoplay);
+        console.log('Mouseenter detected');
+    };
+
+    // Mouseleave Event
+    CaroS.onmouseleave = () => {
         clearInterval(CaroAutoplay);
         CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
-        console.log(e.type + 'mouse detected');
-        }
+        console.log('Mouseleave detected');
+    };
 
-    //------- Mouseclick event
-    CaroS.onclick = function(e) {
+    // Mouseclick event
+    CaroS.onclick = () => {
         clearInterval(CaroAutoplay);
-        console.log(e.type + 'mouse detected');
-    }
+        console.log('Mouse click detected');
+    };
 
-    //------- Gesture Tap Event
-    hammer.on('tap', function(e) {
+    // Gesture Tap Event
+    hammer.on('tap', () => {
         clearInterval(CaroAutoplay);
-        console.log(e.type + 'gesture detected');
+        console.log('Tap gesture detected');
     });
 
-    //----- Gesture Swipe Event
-    hammer.on('Swipe', function(e){
+    // Gesture Swipe Event
+    hammer.on('swipe', () => {
         clearInterval(CaroAutoplay);
         CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
-        console.log(e.type + 'gesture detected');
+        console.log('Swipe gesture detected');
     });
 
     let slideLink = document.querySelectorAll('.slider-item');
-    if (slideLink && slideLink !== null && slideLink.length > 0){
+    if (slideLink) {
         slideLink.forEach(el => el.addEventListener('click', e => {
-        e.preventDefault();
-        let href = el.dataset.href;
-        let target = el.dataset.target;
-        if (href !== '#') window.open(href, target);
-    }));
-//---- Slider End
-
-
+            e.preventDefault();
+            let href = el.dataset.href;
+            let target = el.dataset.target;
+            if (href !== '#') window.open(href, target);
+        }));
+    }
+    // Slider End
 });
